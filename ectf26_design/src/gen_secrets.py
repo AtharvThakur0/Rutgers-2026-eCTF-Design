c8f8c267a13833984749ab3dd126969b1bf74f6c
Puyang He <128939003+Starscreen2@users.noreply.github.com>
2026-02-04 11:32:45 -0500

    tools: add binary secret generation utility and header emitter

--- ectf26_design/src/gen_secrets.py
+++ ectf26_design/src/gen_secrets.py
@@ -10,11 +10,15 @@ own risk!
 Copyright: Copyright (c) 2026 The MITRE Corporation
 """
 
+import os
+import secrets
 import argparse
+import struct
 import json
 from pathlib import Path
-
-from loguru import logger
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives import serialization 
 
 
 def gen_secrets(groups: list[int]) -> bytes:
@@ -27,27 +31,54 @@ def gen_secrets(groups: list[int]) -> bytes:
     All generated secrets must be contained in the returned bytes
     object.
 
+    ===== NOT USED ======
     :param groups: List of permission groups that will be valid in this
-        deployment.
+        deployment. 
 
     :returns: Contents of the secrets file
     """
-    # TODO: Update this function to generate any system-wide secrets needed by
-    #   your design
+    
+    private_key = ec.generate_private_key(ec.SECP256R1())
+    public_key = private_key.public_key()
 
-    # Create the secrets object
-    # You can change this to generate any secret material
-    # The secrets file will never be shared with attackers
-    secrets = {
-        "groups": groups,
-        "some_secrets": "EXAMPLE",
-    }
 
-    # NOTE: if you choose to use JSON for your file type, you will not
-    # be able to store binary data, and must either use a different file
-    # type or encode the binary data to hex, base64, or another type of
-    # ASCII-only encoding
-    return json.dumps(secrets).encode()
+    # save the ECC private key to host
+    # in docker, make sure to save to persisitent volume
+
+
+    private_value = private_key.private_numbers().private_value
+    private_key_bytes = private_value.to_bytes(32, byteorder='big')
+
+    serialized_public = public_key.public_bytes(
+        encoding=serialization.Encoding.X962,
+        format=serialization.PublicFormat.UncompressedPoint
+    )
+
+    aes_key = secrets.token_bytes(32)
+
+    # Binary layout (must match secure_crypto_root_secret_from_global_secrets):
+    #   [  0 ..  64]  ECC public key  (65 bytes, X962 uncompressed)
+    #   [ 65 ..  96]  ECC private key (32 bytes, big-endian scalar)
+    #   [ 97 .. 128]  K_master        (32 bytes, random AES key)
+    #   [129 ..    ]  group list      (2 bytes big-endian per group ID)
+
+    # groups to bytes
+    group_list= [] 
+
+    for group in groups:
+        group_list.append(struct.pack('>H', group))
+
+    group_bytes = b"".join(group_list)
+
+    output = serialized_public + private_key_bytes + aes_key + group_bytes 
+
+    return output
+
+
+
+
+
+
 
 
 def parse_args():
@@ -84,11 +115,7 @@ def main():
 
     secrets = gen_secrets(args.groups)
 
-    # Print the generated secrets for your own debugging
     # Attackers will NOT have access to the output of this, but feel free to remove
-    #
-    # NOTE: Printing sensitive data is generally not good security practice
-    logger.debug(f"Generated secrets: {secrets}")
 
     # Open the file, erroring if the file exists unless the --force arg is provided
     with open(args.secrets_file, "wb" if args.force else "xb") as f:
@@ -96,8 +123,9 @@ def main():
         f.write(secrets)
 
     # For your own debugging. Feel free to remove
-    logger.success(f"Wrote secrets to {str(args.secrets_file.absolute())}")
+    # logger.success(f"Wrote secrets to {str(args.secrets_file.absolute())}")
 
 
 if __name__ == "__main__":
     main()
+
